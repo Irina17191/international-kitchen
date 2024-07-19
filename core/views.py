8c1bf22d5215653e32a7e3ae85f09ace3ed0929a
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-07-19 13:33:10 +0300

    Create url and view for index page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from core.models import Cook, Dish, DishType, Country
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dishtypes = DishType.objects.count()
+    num_countries = Country.objects.count()
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dishtypes": num_dishtypes,
+        "num_countries": num_countries,
+    }
+
+    return render(request, "core/index.html", context=context)
