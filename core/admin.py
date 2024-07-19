589e2f119f4be8880aad290b585abb968ce127e0
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-07-19 12:40:41 +0300

    Set up models for admin panel

--- core/admin.py
+++ core/admin.py
@@ -3,8 +3,34 @@ from django.contrib.auth.admin import UserAdmin
 
 from core.models import DishType, Dish, Country, Cook
 
-
 admin.site.register(DishType)
-admin.site.register(Dish)
 admin.site.register(Country)
-admin.site.register(Cook, UserAdmin)
+
+
+@admin.register(Cook)
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                        "country"
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("dish_type",)
