2baabba71181e2aa048615707912c71b03e35f29
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-07-19 15:14:02 +0300

    Connect static files

--- international_kitchen/urls.py
+++ international_kitchen/urls.py
@@ -23,4 +23,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls", namespace="core")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
