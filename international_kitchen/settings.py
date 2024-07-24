a29de5d344187312219fa3496befd0e02bb6a438
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-07-24 20:04:58 +0300

    Remove insecure django secret key for deployment

--- international_kitchen/settings.py
+++ international_kitchen/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-gw0m(l#e*=^8c5-kpt-#$9vf7=_4jcso50$-(v4!5w^$ift3&&")
+# SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-gw0m(l#e*=^8c5-kpt-#$9vf7=_4jcso50$-(v4!5w^$ift3&&")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
